637e471d234da4d7110b2bbd6888de98077c0740
Alan Houston <alanhouston5000@gmail.com>
2019-04-02 18:26:22 -0500

    fonts, spacing

--- src/Home.js
+++ src/Home.js
@@ -3,13 +3,14 @@ import React from 'react'
 function Home(props){
     return (
         <div className="App">
-            <button className="buttonStyle" onClick={()=>props.changeView(1)}>Pull Ups!</button>
+            <button className="buttonStyle" id="topButton" onClick={()=>props.changeView(1)}>Pull Ups!</button>
             <br/>
             <button className="buttonStyle" onClick={()=>props.changeView(2)}>Push Ups!</button>
             <br/>
-            The Armstrong Program HomePage:
+            <div id="infoStyle">For more detailed info and instructions, </div>
+            <div>visit the Armstrong Pullup Program home page:</div>
             <br/>
-            <a className="homeButton" href="http://armstrongpullupprogram.com/" target="_blank">Armstrong Program</a>
+            <a className="homeButton" id="armHomeStyle" href="http://armstrongpullupprogram.com/" target="_blank">Armstrong Program</a>
             {/* <br/>
             <button onClick={()=>props.changeView(2)}>Stats!</button> */}
         </div>
